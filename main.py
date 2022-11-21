6525339d89ccebb71caaa1eb783b4d135b34f671
Aakansha2663 <aakanshagautam12@gmail.com>
2022-11-21 15:27:01 +0545

    added print

--- main.py
+++ main.py
@@ -1,19 +1,15 @@
 ## main python file
-<<<<<<< HEAD
 from division import division
-=======
 from addition import add_numbers
->>>>>>> 3c51653d15897719f899f7d282cd6ebed7f92189
 
 
 def calculate():
     print('Output Here')
-<<<<<<< HEAD
     print(division(4,2))
-=======
     print(add_numbers(5,10))
 
->>>>>>> 3c51653d15897719f899f7d282cd6ebed7f92189
+    print("Output ends here")
+
 if __name__ == "__main__":
     calculate()
  
